132030c054b6588d437cb9df99262e98542700cb
RashmiMuraharishetti <rashmi.muraharishetti@gmail.com>
2018-12-14 11:02:37 -0700

    first commit

---
+++ src/main/java/com/assessment/TheNZT/TheNztApplication.java
@@ -0,0 +1,14 @@
+package com.assessment.TheNZT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheNztApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheNztApplication.class, args);
+	}
+
+}
+
